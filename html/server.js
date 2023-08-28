18458ac6148f2d551e5900d2610173c9c5f45705
mirroringcode <miguelolivar999@gmail.com>
2023-08-28 15:36:47 -0400

    first commit

---
+++ html/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/assets', express.static('assets'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {
+        root: __dirname,
+    });
+});
+
+app.listen(3000, () => console.log("server on port 3000"));
